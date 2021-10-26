7e3d08217f36d4ffc9873c0eaa80587a79e91537
umamahale <umagasmos@gmail.com>
2021-10-26 09:24:55 -0400

    first message

---
+++ TicTacToeGame.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TIC_TAC_TOE
+{
+    class TicTacToeGame
+    {
+        static char[] arr = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', };
+        static int player = 1;
+        static int 
+
+         static int flag = 0;
+        static void Main (string[] args)
+        {
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("player1: X and Player2 : B");
+                Console.WriteLine("\n");
+            }
+        }
+
+       
+
+            
+
+    }
+}
